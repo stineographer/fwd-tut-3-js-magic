93150c50cb6ac451252a0ea169fc214e202b6ee2
stineographer <christine.metamoment@gmail.com>
2022-08-09 19:27:12 -0400

    writing a better novel inside the JS
    -the great Canadian novel inside the JS

--- index.js
+++ index.js
@@ -136,14 +136,16 @@ function printPhoneBook(passedInPhoneBook) {
 //The argument for our sequentialSearch
 // is the input_id of the html input element. We're using this to store the value of the user's input.
 function sequentialSearch(input_id) {
-  //TIRED, NEED TO EAT SOME FOOD BUT NEXT TIME TO DO:
-  //THIS STUFF BELOW IS A HUGE MESS, FLIPPING FIX IT CHRISTINE!! You picked the messiest possible way to display result messages!
-  //Figure out the correct number of possible outcomes!
-  const firstOutcome = "AND the winner is... (drum-roll, please) our MAX value! " + phoneBook[8];
-  const outcomeMssages = [firstOutcome, "sorry, yer number ain't in here! the good news is here's the last spot we had to check: ", "indx++", "winning index ="]
-  // Above is an array storing one string message for each outcome.
+  //When it comes to an efficient algorithm, you have to really think about what the goal is.
+  //We're trying to complete this task in as few steps as possible,
+  //WHILE using up as few resources as possible.
+  //That means, we'll take a couple well-thought out short-cuts.
+
+  //Any time you accept input from a user, you'll need to provide that user with some kind of output.
+  //We'll be displaying this output with the help of a CSS class.
   const outcomeElem = document.querySelector(".outcome");
   //The html element for our outcomeMessages is designated by this 'outcome' class.
+  //We'll be using this element to display more html elements, along with strings (text data).
 
   // Now we'll get our input from the user.
 // The input must be numerical.
@@ -152,6 +154,10 @@ function sequentialSearch(input_id) {
 //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
   const oElem = document.getElementById(input_id);
   // We're picking out the element containing an input string from the user.
+
+  //The first set of short-cuts involves checking our input thoroughly before we even start any loop iterations.
+  //Why bother entering a loop if you don't need to?
+
   //Then we use a JavaScript function, called parseInt
   //to turn the input string into an integer.
   //The second paramter (where we place our argument, 10)
@@ -170,21 +176,15 @@ function sequentialSearch(input_id) {
 //If it's true that our target value isn't a number then we can't go any further,
   //so we stop here and ask the user to try again.
 
-  //If we make it past the above check, then we know our targetValue is a number.
-  // We'll clear out whatever's in this element.
-   outcomeElem.innerHTML = " "
-
+  //At this point in the code, we know that our input is a number (we made it past that IF statement!)
   //There's a lot going on in this next line of JS.
   const distance = parseInt((phoneBook[8] - targetValue)/(10**6));
-  //We're assigning a value: the distance our target value is
-  //from either the maximum or minimum value in our phoneBook,
-  // and we're storing that distance value as an integer.
-
-  //This distance is an absolute value because we only want a positive distance.
-  //Remember, I'm using the distance from the maximum value in our array
-  //and the sort is in ascending order.
-  //That means the maximum value is in the phoneBook's last spot,
-  //where the index equals 8.
+  //Because ORDER matters in this list, we're going to estimate the best spot to start checking for our value.
+  //We'll do that by getting the DISTANCE our targe is from the MAXIMUM value,
+  //which we know is the last value in the list because it's sorted in ASCENDING order.
+  //We're assigning a constant value, and calling it "distance": the distance our target value is
+  //from the maximum value in our phoneBook,
+  // and we're making sure that value is stored as an integer (by using parseInt).
 
   //At the very end of the above JS line,
   //you'll see that we're dividing our distance by 10 power 6.
@@ -193,41 +193,48 @@ function sequentialSearch(input_id) {
   //is in the same scale as the index of our array.
   //We want to use our phonBook array index to determine the best spot to start our search.
 
+  //Hey, life is short! Why start at zero?
     let index;//Here, we're declaring a variable
 //that will allow us to track what spot we're checking in
 //our phoneBook.
 //We'll initialize, or assign, the value in a separate statement.
 
-  //-now that we've got our distance,
-      //the first thing we're gonna check is whether
+  //Before we go any further, we're going to consider possible outcomes.
+  //Either the target value is in the list or it's not.
+  //If it's NOT in the list, we'll tell the user after checking as few spots as possible.
+  //If the target value is in the list we'll still check as few values as possible,
+  //then when we find the target we'll tell the user where we found it (return the index).
+
+  const outcomeMssages = ["sorry, yer number ain't in here! the good news is here's the last spot we had to check: ", "we found it!", "Look at us! We're iterating cuz the target is bigger than the value here! indx++ ", "winning index ="]
+  // Above is an array storing one string message for each outcome.
+
+  //I'm not sure if I've told you how expensive comparisons are, you might as well know now.
+  //The price of comparisons has really sky-rocketed, and once you put them in a loop... Well, lets just say they can get outta control.
+  //That's why we're going to do a bunch of comparisons before we even enter a loop
+  //because these comparisons will give us an idea of the best spot to start looking for our target value.
+
+  //the first thing we're gonna check is whether
   if(distance <= 0){
       if(distance==0){
-         outcomeElem.innerHTML += outcomeMssages[0] + "<br/>";
-           //-cuz it's possible, so you gotta consider it
-    //-you may have just won because the phone number you entered is the first value you've checked!
+         outcomeElem.innerHTML += outcomeMssages[1] + "<br/>";
+        //-cuz it's possible, so you gotta consider it
+          //-you may have just won because the phone number you entered is the first value you've checked!
     //-everyone gets to go home early!
     //-we're outta here!
             //-this is exciting!
     //-we don't even have to enter the while loop!
       }
-      else{
+      else{//Thanks to some of the data validation we put in the html, this check for positive input is unnecessary (hint: line #150 in the html!)
          outcomeElem.innerHTML += "Are you kidding? That number's way too big for this list! <br/>";
       }
 
-  }
+  }//end winner and positive check
   else if (distance >= n){
-      //-ok, so we're down here now
-  // which means we didn't win
-  // -but don't worry, we've still got a sorted list so everyone gets to go home early anyway ;)
-
-       //-we already know that the distance is a positive number,
-    //because we got the absolute value of our MAX - target
-  //-the next thing we're gonna check is:
+  //-the next thing we're checking is:
   // whether the distance >= the number of spots in our Array
-
        index = 0;  //-even in this case, you'll never have to check all the way to the nth spot
-    //-I'll show you why
-  }
+      //-I'll show you why ***
+  }//end the n-check
   else{
     //inside the next line:
     // to get our starting index, we subtract 1 from the length of the array
@@ -235,48 +242,72 @@ function sequentialSearch(input_id) {
     index = ((phoneBook.length-1) - distance);
     //-we want to start checking at the spot we expect the targetValue to be
     //This is where it's important that the index and the distance be on the same scale.
-  }
+  }//end the guesstimation of our starting index
 
   //Our final WHILE loop... for this tutorial, that is.
   //-in case you need a refresher:
 //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
-  while (true) {//so this is a tricky thing that I'm doing here, in ORDER to avoid a comparison
-    //if KNOW that you have an ordered list, then you don't have to do the comparison inside this while
-    //if you're going up, you're increasing the index
-    //if you're going down, then you decrease the index
-    // you don't have to check it against the final value every time the loop executes
+  while (true) {//so this is a tricky thing that I'm doing here in order to avoid a comparison
+    //this is a little bit dangerous... what we're saying here is that we want the loop to continue no matter what.
+    //We're saying, don't even bother checking a condition. Keep going no matter what!
+//don't worry, we'll be putting the BREAKS on very soon.
 
-    //BUT again, in order to do this I'll have to introduce another short-cut: break;
-    //yes, we're putting on the breaks in our loop! instead of actually comparing values
-    //however this is the way algorithms are measured for efficiency: how many comparisons does it need,
-    // and what's the work involved in order to do that comparison
+    //if you KNOW that you have an ordered list, then you don't have to do the comparison inside this while
+    //if you're going up, you're increasing the index.
+    //if you're going down, then you decrease the index.
+    //it's guaranteed that when you increase the index the next value will be larger.
 
-    //in that wikipedia article:
-    //https://en.wikipedia.org/wiki/Linear_search#With_a_sentinel
-    //we're being told that an ordered list requires a sentinel, but as you can see above that's not true!
+    //We have to remember that we're now in very expensive comparison territory.
+    //Every comparison inside here counts not just once, but possibly for every iteration.
+    //That includes the condition used for the WHILE loop itself.
 
-    if(phoneBook[index] > targetValue){//FIRST COMPARISON
-      console.log("compare 1");
+    //You're probably thinking to yourself, Shirley she'll need at least TWO comparisons per iteration!?
+    // that's not necessarily the case when you're able to cut your iteration short,
+    //using BREAKS.
+    //Yes you heard it here first,
+    //we're putting the breaks on our iteration because it'll help us to BREAK the loop without further comparisons.
+
+
+    //FIX EVERYTHING BELOW HERE! Thems THE BREAKS!! (another short cut!)
+
+    //We'll be doing this very carefully, because the whole point is to keep the algorithm efficient.
+    //Once inside this loop there's only 3 possible outcomes, and every one of them counts as a comparison:
+    // 1) target value == index value,
+    //2) target value < index value,
+    //2) target value > index value,
+    //3) . //REMEMBER: this one is least likely!!
+
+    //We're unravelling these outcomes in a specific order that has everything to do with the order of our list.
+    //Before even entering this loop, we ruled out the possibility that the target value matched the maximum
+    //(last) value in our list.
+    //We've set our index to the spot where we expect our target value to go.
+    //We're also expecting our target value to be greater than the index value we've chosen. (I'll let you figure that one out ;)
+    //Because the list is in ASCENDING order, the first comparison will be
+    // drum-roll please...
+
+    if(targetValue < phoneBook[index]){//FIRST COMPARISON
+      //Think about it this way, how did we get the 'distance' variable?
+      //We subtracted it from the MAX value.
+      //It should be greater than or equal to the value in this spot.
+      //If it's the case that the target is less than this value, then we know the target is not in this list.
+
+      //There's no point in checking any further values (even when the list is full of duplicates!)
+      console.log("COMPARE #1) guess what? if your target is less than this: " + phoneBook[index] + ", then you might as well get the flip out!");
       //STOP! -you've lost! -the phone number your looking for is NOT here!
+      //Lets put the breaks on this loop and get out now! We do get to go home early!
            outcomeElem.innerHTML += outcomeMssages[1] + index + "<br/>";
-      //here's some more good news:
-      //-I'm betting this index is NEVER equal to n!
-      //-this is where you get to try and prove me wrong!
       break;
-      //-once again, Christine is using a break short-cut
-      //-this is where we're putting the brakes on our WHILE loop
-      // - because otherwise it'll just keep going
-      // Can you figure out why?
+    }//-end FIRST COMPARISON
 
-    }//-end first condition
     else if(targetValue > phoneBook[index]){//SECOND COMPARISON
       //Wanna learn more about the else if in JS? https://www.w3schools.com/js/js_if_else.asp
-      console.log("compare 2");
+            //This is kinda what we're expecting. We picked this spot because we expect our target to be at least as big as the value here. If that's NOT true, then there's only one other possibility, right?
+      console.log("COMPARE #2) You gotta be at least this big if you want to iterate through the rest of this list: " + phoneBook[index]);
       outcomeElem.innerHTML += outcomeMssages[2] + index + "<br/>";
        index++;
-
+      // Here we're increasing the index without checking (comparing) its value, cuz that's real expensive! So why do it?
     }//end else if
-    else {//we've reached the end of the list! we're done!
+    else {//we've r
       console.log("it's the end of the line, folks! please exit in an ORDERly fashion, even if you're not at all fashionable.");
 
       if(targetValue == phoneBook[index]){//here's the winning location!
